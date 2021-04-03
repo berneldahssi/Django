28c3e4ce1a36b73ede41651da02a43eb11bde2bc
berneldahssi <44126034+berneldahssi@users.noreply.github.com>
2021-04-03 09:37:18 +0100

    Delete NORACO directory

--- NORACO/NORACO/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404
-
-def home(request):
-    return render(request, 'pages/home.html')
-def about(request):
-    return render(request, 'pages/about.html')
--- NORACO/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-app_name='blog'
-urlpatterns = [
-    url('', views.index, name='index'),
-    url('posts/<int:id>-<slug:slug>', views.show, name='show'),
-    url('contact/', views.contact,  name='contact'),
-]
